cc1cbc88ac402b646c613d0b8ad392b9eeaeefa5
Amreeta Choudhury <amreeta89@yahoo.com>
2016-07-31 16:02:35 -0400

    Changed a file for security

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Turkey burger");
+INSERT INTO burgers (burger_name) VALUES ("Philly Cheesesteak");
+INSERT INTO burgers (burger_name) VALUES ("Double Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
