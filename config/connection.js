e9cc5a4a443f662e3b09c3104ad672921c4fae3e
Amreeta Choudhury <amreeta89@yahoo.com>
2016-08-04 14:42:41 -0400

    Fixed schema.sql. Table was already saved but code wasn't

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 //Connection to the database
 
 var mysql = require('mysql');
+var conn2 = require('./connection2.js');
+
+console.log(conn2.password);
+
+
 var connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
 		user: 'root',
-		password: '',
+		password: conn2.password,
 		database: 'burgers_db'
 });
 
