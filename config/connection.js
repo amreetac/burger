04cb49cfe356b26222679f60c9f732c0fb75af91
Amreeta Choudhury <amreeta89@yahoo.com>
2016-08-06 15:08:12 -0400

    Re-did connection.js and got authentication for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,36 @@
-//Connection to the database
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-		port: 3306,
-		host: 'localhost',
-		user: 'root',
-		password: 'Smithsonian123',
-		database: 'burgers_db'
-});
+// Edit this connection to make it work with JawsDB.
+var source = {
 
-connection.connect(function (err){
-		if (err) {
-						console.error('error containing:' + err.stack);
-						return;
-		}
-		console.log('connected as id' + connection.threadId);
+    jawsDB: {
+        port: 3306,
+        host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'w93igidhzk0ickaw',
+        password: 'v3i668i9p5z0knbn',
+        database: "l9r6djimr777seo1"
+
+        // TODO: Add your JawsDB connection info here
+    }
+}
+
+
+// TODO: connect your jawsDB connection witht he mysql connection.
+var connection = mysql.createConnection(source.jawsDB); //enter connection here);
+
+
+
+// Replace the connection code here with one to connect to you JawsDB connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
